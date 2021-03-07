05ebcec6eba5f04f85e27a4b31dd053bbbac62ea
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-07 09:08:10 -0800

    style

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 export default function NavBar(){
   return (
-    <div className="nav bg-real-dark ">
+    <div className="container-lg navbar">
+      <h1 className="navbar-brand">Nick Levenson</h1>
       <div className="nav float-end">
         <NavLink to="/" className="nav-link">Home</NavLink>
         <NavLink to="/projects" className="nav-link">Projects</NavLink>
