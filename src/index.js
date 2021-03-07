c0647aaa378846f2a925f5985e62365a65f4a619
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-06 17:25:56 -0800

    gh=pages

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import {appReducer} from './reducers/appReducer'
 
 
-const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  
-   <App/>
-  ,
+  <App/>,
   document.getElementById('root')
 );
 
