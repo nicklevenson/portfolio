f979906191b03cb3ebf1a553870a1914acb61cf4
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-07 18:03:52 -0800

    display

--- src/components/project.js
+++ src/components/project.js
@@ -6,17 +6,19 @@ export default class Project extends React.Component {
   render() {
     console.log(this.project.img)
     return(
-      <Card style={{ width: '18rem' }}>
-      <Card.Img src={this.project.img}/>
-      <Card.Body>
-        <Card.Title>{this.project.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <div className="d-inline-block m-5">
+        <Card style={{ width: '25em' }}>
+        <Card.Img src={this.project.img}/>
+        <Card.Body>
+          <Card.Title>{this.project.name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
     )
   }
 }
--- src/containers/projects.js
+++ src/containers/projects.js
@@ -43,10 +43,12 @@ export default class Projects extends React.Component {
   render() {
     return (
 
-      <div id="projects" className="mx-auto col-8 d-table vh-100">
+      <div id="projects" className="mx-auto col-12 d-table vh-100">
         <h1>Projects</h1>
-        <div className="d-flex p-5">
-          {this.state.projects.map(p => <Project project={p}/>)}
+        <div className="row w-100">
+          <div className="col-12 d-inline-block">
+            {this.state.projects.map(p => <Project project={p}/>)}
+          </div>
         </div>
       </div>
     )
