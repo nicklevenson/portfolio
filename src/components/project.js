857fd7a9c95a1f034f895cb79b95d165b7b2ba11
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-08 11:19:33 -0800

    links

--- src/components/project.js
+++ src/components/project.js
@@ -12,35 +12,35 @@ export default class Project extends React.Component {
         <br/>
        
           <Card className="bg-darkish text-white"style={{ width: '25em', maxWidth: "75vw", margin: "auto" }}>
-            <Card.Img src={this.project.img}></Card.Img>
-           
-              
-              <h2>{this.project.name}</h2>
+              <a href={this.project.url} target="_blank">
+                <Card.Img src={this.project.img}></Card.Img>
+                <h2>{this.project.name}</h2>
+              </a>
               <Card.Text>
                 <i>{this.project.logline}</i>
               </Card.Text>
 
               <Accordion>
                 <Accordion.Toggle as={Card.Text} eventKey="1">
-                  <h5 className="text-center">Technologies Used ▽</h5>
+                  <h5 className="text-center m-0">Technologies Used ▽</h5>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
-                  <Card.Body>
-                    {this.project.descriptions}
-                  </Card.Body>
+
+                    <p>{this.project.descriptions}</p>
+                  
                 </Accordion.Collapse>
-                <div class="">
-                <a>Website</a>
-                <a>Video Demo</a>
-                <a>Blog Post</a>
-                <a>Github</a>
-                </div>
               </Accordion>
      
-            <div class="iframe-container">
-              {/* <iframe className="iframe" src={this.project.video} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> */}
-        
-            </div>
+                  <Card.Footer className="mt-auto">
+                    <div className="d-inline-block p-2"><a href={this.project.url} target="_blank">Website</a></div>
+                    <div className="d-inline-block p-2"><a href={this.project.video} target="_blank">Video Demo</a></div>
+                    <div className="d-inline-block p-2"><a href={this.project.blog} target="_blank">Blog Post</a></div>
+                    <div className="d-inline-block p-2"><a href={this.project.github} target="_blank">Github</a></div>
+                  </Card.Footer>
+                
+                
+              
+          
       
           </Card>
      
