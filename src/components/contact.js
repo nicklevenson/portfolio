442df7131ce564ee3d8af7153db8d1e53cc46dfe
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-09 15:28:12 -0800

    contact mod

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,11 +10,11 @@ export default class Contact extends React.Component {
       <div id="contact" className="w-100 d-table bg-light-red">
         <div className="contact box  mx-auto d-table-cell align-middle">
           <h1>Let's Get In Touch</h1>
-          <a href="mailto:nicklevenson@gmail.com?subject=Reaching Out to Nick" className="m-5"><img src={mail}style={{ width: '12em', margin: "auto"}}></img></a>
-          <a href="https://www.linkedin.com/in/nicholas-levenson-b3a927b2/" className="m-5" target="_blank"><img src={linkedin}style={{ width: '10em', margin: "auto"}}></img></a>
+          <a href="mailto:nicklevenson@gmail.com?subject=Reaching Out to Nick" className="m-5"><img src={mail}style={{ width: '5em', margin: "auto"}}></img></a>
+          <a href="https://www.linkedin.com/in/nicholas-levenson-b3a927b2/" className="m-5" target="_blank"><img src={linkedin}style={{ width: '4em', margin: "auto"}}></img></a>
           <br/>
           <br/>
-          <h1>971-275-2967</h1>
+         
         </div>
       </div>
     )
