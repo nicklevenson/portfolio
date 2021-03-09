67a90a879c5dda7898c479796fa1a461b8f27181
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-09 15:03:57 -0800

    nav style

--- src/navbar.js
+++ src/navbar.js
@@ -6,52 +6,55 @@ import dev from "./assets/dev.png"
 export default function NavBar(){
   
   return (
-    <div className="navbar w-100 px-5 navbar-expand-lg sticky-top bg-white">
-      <div className="navbar-brand nav">NICK LEVENSON</div>
-        <a><img src={github} className="nav-icon nav-item"></img></a>
-        <a><img src={linkedin} className="nav-icon nav-item"></img></a>
-        <a><img src={dev} className="nav-icon nav-item"></img></a>
-    
-      <div className="w-100 text-right">
-        <Link
-          className="nav-item"
-          activeClass="active"
-          to="home"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >Home</Link>
-        <Link
-          className="nav-item"
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        
-        >Projects</Link>
-        <Link
-          className="nav-item"
-          activeClass="active"
-          to="blog"
-          spy={true}
-          smooth={true}
-          offset={-80}
-          duration={500}
-        >Blog</Link>
-        <a className="nav-item">Resume</a>
-        <Link
-          className="p-1 nav-item"
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-80}
-          duration={500}
-        >Contact</Link>
-      </div>
+      <div className="navigation bg-white w-100 px-5" >
+        <div className="header">
+          <div className="title d-inline-block">NICK LEVENSON </div>
+          <div className="social-links d-inline-block">
+            <a><img src={github} className="nav-icon "></img></a>
+            <a><img src={linkedin} className="nav-icon "></img></a>
+            <a><img src={dev} className="nav-icon "></img></a>
+          </div>
+        </div>
+        <div className="nav-links">
+          <Link
+            className="page-links"
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >Home</Link>
+          <Link
+            className="page-links"
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-110}
+            duration={500}
+          
+          >Projects</Link>
+          <Link
+            className="page-links"
+            activeClass="active"
+            to="blog"
+            spy={true}
+            smooth={true}
+            offset={-90}
+            duration={500}
+          >Blog</Link>
+          <a className="page-links">Resume</a>
+          <Link
+            className="page-links"
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-90}
+            duration={500}
+          >Contact</Link>
+        </div>
     </div>
    )
 }
