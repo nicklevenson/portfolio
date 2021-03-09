737560071af166057a04563bd144ec606e8443df
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-09 13:22:16 -0800

    favicon

--- src/navbar.js
+++ src/navbar.js
@@ -6,53 +6,52 @@ import dev from "./assets/dev.png"
 export default function NavBar(){
   
   return (
-    <div className="navbar w-100 navbar-expand-lg sticky-top bg-white">
-      
-        <div className="navbar-brand nav">NICK LEVENSON</div>
-          <a ><img src={github} className="nav-icon nav-item"></img></a>
-          <a > <img src={linkedin} className="nav-icon nav-item"></img></a>
-          <a > <img src={dev} className="nav-icon nav-item"></img></a>
+    <div className="navbar w-100 px-5 navbar-expand-lg sticky-top bg-white">
+      <div className="navbar-brand nav">NICK LEVENSON</div>
+        <a><img src={github} className="nav-icon nav-item"></img></a>
+        <a><img src={linkedin} className="nav-icon nav-item"></img></a>
+        <a><img src={dev} className="nav-icon nav-item"></img></a>
     
-     
-      <Link
-        className="nav-item"
-        activeClass="active"
-        to="home"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}
-      >Home</Link>
-       <Link
-        className="nav-item"
-        activeClass="active"
-        to="projects"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}
-       
-      >Projects</Link>
-      <Link
-        className="nav-item"
-        activeClass="active"
-        to="blog"
-        spy={true}
-        smooth={true}
-        offset={-80}
-        duration={500}
-      >Blog</Link>
-      <a className="nav-item">Resume</a>
-      <Link
-        className="p-1 nav-item"
-        activeClass="active"
-        to="contact"
-        spy={true}
-        smooth={true}
-        offset={-80}
-        duration={500}
-      >Contact</Link>
-
+      <div className="w-100 text-right">
+        <Link
+          className="nav-item"
+          activeClass="active"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >Home</Link>
+        <Link
+          className="nav-item"
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        
+        >Projects</Link>
+        <Link
+          className="nav-item"
+          activeClass="active"
+          to="blog"
+          spy={true}
+          smooth={true}
+          offset={-80}
+          duration={500}
+        >Blog</Link>
+        <a className="nav-item">Resume</a>
+        <Link
+          className="p-1 nav-item"
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-80}
+          duration={500}
+        >Contact</Link>
+      </div>
     </div>
    )
 }
