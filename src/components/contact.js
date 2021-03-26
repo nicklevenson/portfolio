cbef3f26b5b3a898e4ad4c6169a3a546622e217d
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-25 18:24:48 -0700

    hiehgt

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ export default class Contact extends React.Component {
     
         <div id="contact" className="w-100 d-table bg-light-red">
           <div className="contact box  mx-auto d-table-cell align-middle">
-          <ScrollAnimation animateIn="animate__fadeIn" animateOut="animate__fadeOut" offset={250} >
+          <ScrollAnimation animateIn="animate__fadeIn" animateOut="animate__fadeOut" offset={150} >
             <h1>Let's Get In Touch</h1>
             <a href="mailto:nicklevenson@gmail.com?subject=Reaching Out to Nick" className="m-5"><img src={mail}style={{ width: '5em', margin: "auto"}}></img></a>
             <a href="https://www.linkedin.com/in/nicholas-levenson-b3a927b2/" className="m-5" target="_blank"><img src={linkedin}style={{ width: '4em', margin: "auto"}}></img></a>
